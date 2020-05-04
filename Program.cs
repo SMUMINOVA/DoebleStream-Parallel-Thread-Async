ba93d5a6fe016c1f74623b572ca6f306a81b988a
Sobira <sobira204@gmail.com>
2020-05-04 23:27:40 +0500

    create file description.txt

--- Program.cs
+++ Program.cs
@@ -4,54 +4,52 @@ using System.Threading;
 
 namespace task
 {
-    class Program
+     class Program
     {
         static object locker = new Object(); 
-        static int GlobalLength = 20;
+        static int GlobalLength = 30;
         static int flag = 0;
         static void Main(string[] args)
         {
             Parallel.ForEach(new int[]{1,3,5,7,9,11,13,15,17,19}, DoubleStream);
-            //Stream(2);
-
             Console.ReadKey();            
         }
         static void Stream(int left){
-            int limit1 = new Random().Next(3,5);
-            int fixLim = limit1; 
-            int top1 = 0;
-            int makeEmpty = top1;
+            int limit = new Random().Next(3,5);
+            int fixLim = limit; 
+            int top = 0;
+            int makeEmpty = top;
             int deleteFrom = GlobalLength;
             while(true){
                 lock(locker){
-                    if(top1 + limit1 >= GlobalLength){
-                        limit1 = GlobalLength - top1;
-                        if(limit1 < 0){
-                            limit1 = fixLim;
-                            top1 = 0;
+                    if(top + limit >= GlobalLength){
+                        limit = GlobalLength - top;
+                        if(limit < 0){
+                            limit = fixLim;
+                            top = 0;
                         }    
                     }                
-                    makeEmpty = top1;
+                    makeEmpty = top;
                     for (int i = deleteFrom; i < makeEmpty; i++){
                         Console.CursorTop = i;
                         Console.CursorLeft = left;
                         System.Console.WriteLine(" ");
                     }
-                    if(top1 == 0){
+                    if(top == 0){
                         for (int i = deleteFrom; i < GlobalLength; i++){
                             Console.CursorTop = i;
                             Console.CursorLeft = left;
                             System.Console.WriteLine(" ");
                         }
                     }
-                    for(int i = 0; i < limit1; i++){
-                        if(i == (limit1 - 1))
+                    for(int i = 0; i < limit; i++){
+                        if(i == (limit - 1))
                             Console.ForegroundColor = ConsoleColor.White;
-                        else if(i == (limit1 - 2))
+                        else if(i == (limit - 2))
                             Console.ForegroundColor = ConsoleColor.Green;
                         else 
                             Console.ForegroundColor = ConsoleColor.DarkGreen;
-                        Console.CursorTop = top1++;
+                        Console.CursorTop = top++;
                         Console.CursorLeft = left;
                         switch(new Random().Next(0,15)){
                                 case 0: System.Console.WriteLine("A");break;
@@ -72,10 +70,10 @@ namespace task
                                 case 15: System.Console.WriteLine("9");break;
                             }
                     }
-                    deleteFrom = top1 - limit1;
-                    top1 = makeEmpty + new Random().Next(2,4);
+                    deleteFrom = top - limit;
+                    top = makeEmpty + new Random().Next(2,4);
                 }
-                if(deleteFrom >= 7 && flag == 0){
+                if(deleteFrom >= 8 && flag == 0){
                     Task newTask = new Task(() => Stream(left));
                     newTask.Start();
                     flag = 1;
